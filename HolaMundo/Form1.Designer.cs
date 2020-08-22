c49349648c6e1ca206fbde7680d71fad6e1046d6
David Castaneda <david.castaneda@udea.edu.co>
2020-08-21 23:31:54 -0500

    Primer programa en csharp

---
+++ HolaMundo/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace HolaMundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnsaludar = new System.Windows.Forms.Button();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(336, 113);
+            this.label1.Margin = new System.Windows.Forms.Padding(7, 0, 7, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(316, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mi primer programa";
+            // 
+            // btnsaludar
+            // 
+            this.btnsaludar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.btnsaludar.Location = new System.Drawing.Point(343, 280);
+            this.btnsaludar.Name = "btnsaludar";
+            this.btnsaludar.Size = new System.Drawing.Size(223, 104);
+            this.btnsaludar.TabIndex = 1;
+            this.btnsaludar.Text = "Saludar";
+            this.btnsaludar.UseVisualStyleBackColor = false;
+            this.btnsaludar.Click += new System.EventHandler(this.btnsaludar_Click);
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.BackColor = System.Drawing.Color.Green;
+            this.txtnombre.Location = new System.Drawing.Point(297, 207);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(355, 44);
+            this.txtnombre.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(1778, 833);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.btnsaludar);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "HolaMundo";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnsaludar;
+        private System.Windows.Forms.TextBox txtnombre;
+    }
+}
+
